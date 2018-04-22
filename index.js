e198f75d7f98253982e4a9cca25a40df1e97e660
Jonas Jensen <jonas.jensen@greenbird.com>
2018-04-22 21:40:39 +0200

    format date only for D0055 flow

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const path = require('path');
 const util = require('util');
 const flatten = require('array-flatten');
 const readMultipleFiles = require('read-multiple-files');
-const { columnHeaders } = require('./headers');
 
 const readdir = util.promisify(fs.readdir);
-const writeStream = fs.createWriteStream('./D0055.csv');
 const flowsDirectory = path.join(__dirname, 'flows');
 
 const getFlowDirectories = async flowDirectoryName => {
@@ -20,14 +18,16 @@ const getFlowDirectories = async flowDirectoryName => {
   return flatten(flowFiles);
 };
 
-const formatFlow = contents => {
+const formatFlow = (flowName, contents) => {
   const rawColumns = contents.split('\n')[2].split('|');
 
   // delete 'group id' first column and always-empty last column
   const columns = rawColumns.slice(1, rawColumns.length - 1);
 
-  // format REGI date to Postgres Date
-  columns[3] = [`${columns[3].substring(0, 4)}-${columns[3].substring(4, 6)}-${columns[3].substring(6)}`];
+  if (flowName === 'D0055') {
+    // format REGI date to Postgres Date
+    columns[3] = [`${columns[3].substring(0, 4)}-${columns[3].substring(4, 6)}-${columns[3].substring(6)}`];
+  }
 
   return columns.join(',') + '\n';
 };
@@ -36,18 +36,16 @@ const getFlowsAsCSV = async (flowName, stream) => {
   const files = await getFlowDirectories(flowName);
   readMultipleFiles(files, 'utf8').subscribe({
     next({ contents }) {
-      stream.write(formatFlow(contents));
+      stream.write(formatFlow(flowName, contents));
     }
   })
 };
 
-
 const getAllFlowNames = async () => {
   const datedDirectories = await readdir(flowsDirectory, 'utf8');
   const flowDirectories = await Promise.all(datedDirectories.map(flowDirectory => readdir(path.join(flowsDirectory, flowDirectory), 'utf8')));
-  return Array.from(new Set(flatten(flowDirectories))).filter(directory => directory !== '.DS_Store');
+  return Array.from(new Set(flatten(flowDirectories))).filter(directory => directory.startsWith('D0'));
 };
-
 
 const createFlowWriteStreams = async () => {
   const allFlowNames = await getAllFlowNames();
@@ -56,8 +54,4 @@ const createFlowWriteStreams = async () => {
     .forEach(({ flow, stream }) => getFlowsAsCSV(flow, stream));
 };
 
-
-
-
-// writeStream.write(columnHeaders.join(',') + '\n');
-// getFlowsAsCSV('D0055', writeStream);
+createFlowWriteStreams();
